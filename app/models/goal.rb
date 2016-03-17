f1e7042c06f7dd828ba8e112f7469f1a96e92db9
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2016-03-17 08:53:18 -0500

    adding validation for goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,4 +1,18 @@
 class Goal < ActiveRecord::Base
+# Goal Fields:
+# - string     :title, null: false
+# - text       :description
+# - integer    :frequency
+# - datetime   :deadline
+# - integer    :category
+# - decimal    :target
+# - references :user, index: true
+
+  # Validation
+  validates_presence_of  :title
+  validates_inclusion_of :frequency, in: ["daily", "weekly", "monthly"]
+  validates_inclusion_of :category, in: ["binary", "hourly", "tally"]
+  validates_presence_of  :target
 
   enum category: [
      :binary,
@@ -10,6 +24,7 @@ class Goal < ActiveRecord::Base
      :weekly,
      :monthly ]
 
+  # Associations
   belongs_to :user
   has_and_belongs_to_many :tags
 

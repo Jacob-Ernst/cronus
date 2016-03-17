3ae298a9a83194f7b3fd24067e9beba47b7e3736
Jacob Ernst <jacobernst@Jacobs-MacBook-Air.local>
2016-03-17 08:54:00 -0500

    adding name validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,19 @@
 class User < ActiveRecord::Base
+# User Fields:
+# - string :first_name
+# - string :last_name
+# - string :email, uniqueness: true
+# - string :password_digest
+
 
   has_secure_password
 
   # Validation
   validates :email, presence: true, uniqueness: true
   validates_presence_of :password_digest
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
 
   # Associations
   has_many :goals
